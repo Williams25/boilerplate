a0e8bde9d091c0cf0782f1e1ab3b191a04b21554
Williams25 <william.souza.timao@hotmail.com>
2021-06-21 17:21:24 -0300

    criando componente

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from "./styles";
+
 export const Main = () => {
   return (
-    <main>
-      <h1>React avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e react avançado escrito ao lado"
+      />
+      <S.Title>React avançado</S.Title>
+      <S.Description>TypeScript, React, NextJS</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="um desenvolver de frete ao computador"
+      />
+    </S.Wrapper>
   );
 };
